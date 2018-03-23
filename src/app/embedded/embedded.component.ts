561492929054bfe4faeed73e0141fc00b51ce903
theotherdy <damion@allotmentor.com>
2018-03-23 14:42:08 +0000

    Slider for disease prevalence again

---
+++ src/app/embedded/embedded.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+@Component({
+  selector: 'app-embedded',
+  templateUrl: './embedded.component.html',
+  styleUrls: ['./embedded.component.scss']
+})
+export class EmbeddedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/standalone/standalone.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+@Component({
+  selector: 'app-standalone',
+  templateUrl: './standalone.component.html',
+  styleUrls: ['./standalone.component.scss']
+})
+export class StandaloneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
